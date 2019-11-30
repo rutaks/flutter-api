ab3539f285d0680701a8f65ea99b4f02c34b1057
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-30 12:14:24 +0200

    Created Item Cell

---
+++ lib/views/item_cell.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ItemCell extends StatelessWidget {
+  final item;
+  ItemCell(this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network(item["imageUrl"]),
+              Container(height: 10.0),
+              Text(
+                item["name"],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Container(height: 2.0),
+              Text(
+                "${item['numberOfViews']} Views",
+                style: TextStyle(color: Color.fromRGBO(12, 12, 12, 0.5)),
+              ),
+              Divider(),
+            ],
+          ),
+        )
+      ],
+    );
+    ;
+  }
+}
