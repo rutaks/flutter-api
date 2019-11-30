16b72d2ffd8a5eb445eb68fd502cf886544d1d41
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-30 12:15:31 +0200

    Setup List View From Api Request

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import './views/item_detail.dart';
+import './views/item_cell.dart';
 
 class App extends StatefulWidget {
   @override
@@ -8,6 +12,24 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
+  var _isLoading = false;
+  var videos;
+  final String url = "API ROUTE";
+
+  _fetchData() async {
+    final res = await http.get(url);
+
+    if (res.statusCode == 200) {
+      final map = json.decode(res.body);
+      final jsonVideos = map["videos"];
+
+      setState(() {
+        _isLoading = false;
+        videos = jsonVideos;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -18,10 +40,35 @@ class AppState extends State<App> {
             new IconButton(
               icon: Icon(Icons.refresh),
               onPressed: () {
-                print("object");
+                setState(() {
+                  _isLoading = true;
+                });
+                _fetchData();
               },
             )
           ],
+        ),
+        body: Center(
+          child: _isLoading
+              ? CircularProgressIndicator()
+              : new ListView.builder(
+                  itemCount: videos != null ? videos.length : 0,
+                  itemBuilder: (context, i) {
+                    final video = videos[i];
+                    return FlatButton(
+                      padding: EdgeInsets.all(0.0),
+                      child: ItemCell(video),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => new ItemDetailPage(),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
         ),
       ),
     );
